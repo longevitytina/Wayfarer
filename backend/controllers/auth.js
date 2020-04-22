b585d4d55575ce0cd40055285a6bb477334ede65
Tina <longevity.tina@gmail.com>
2020-04-22 16:04:09 -0700

    Adds login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -66,9 +66,76 @@ const register = (req, res) => {
   });
 };
 
+// POST - login user
+const login = (req, res) => {
+  // console.log(req.body);
+  if (!req.body.email || !req.body.password) {
+    return res
+      .status(400)
+      .json({ status: 400, message: "Please enter your email and password" });
+  }
+
+  db.User.findOne({ email: req.body.email }, (err, foundUser) => {
+    if (err)
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong. Please try again",
+      });
+
+    if (!foundUser) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Email or password is incorrect" });
+    }
+
+    bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+      if (err)
+        return res.status(500).json({
+          status: 500,
+          message: "Something went wrong. Please try again",
+        });
+
+      if (isMatch) {
+        req.session.currentUser = { id: foundUser._id };
+        return res
+          .status(200)
+          .json({ status: 200, message: "Success", data: foundUser._id });
+      } else {
+        return res
+          .status(400)
+          .json({ status: 400, message: "Email or password is incorrect" });
+      }
+    });
+  });
+};
+
+// POST Logout - Destroy Session
+const logout = (req, res) => {
+  if (!req.session.currentUser)
+    return res.status(401).json({ status: 401, message: "Unauthorized" });
+  req.session.destroy((err) => {
+    if (err)
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong. Please try again",
+      });
+    res.sendStatus(200);
+  });
+};
+
+// GET Verify Current User
+// const verify = (req, res) => {
+//   if (!req.session.currentUser)
+//     return res.status(401).json({ status: 401, message: "Unauthorized" });
+//   res.status(200).json({
+//     status: 200,
+//     message: `Current User verified. User ID: ${req.session.currentUser.id}`,
+//   });
+// };
+
 module.exports = {
   register,
-  //   login,
-  //   verify,
-  //   logout,
+  login,
+  verify,
+  logout,
 };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,8 +4,8 @@ const ctrl = require("../controllers");
 
 // PATH = /api/v1/auth
 router.post("/register", ctrl.auth.register);
-// router.post("/login", ctrl.auth.login);
-// router.get("/verify", ctrl.auth.verify);
+router.post("/login", ctrl.auth.login);
+router.get("/verify", ctrl.auth.verify);
 // router.delete("/logout", ctrl.auth.logout);
 
 module.exports = router;
