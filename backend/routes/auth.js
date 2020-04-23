5e7761f507ddcf2216770505e88b3c003b3ff430
Tina <longevity.tina@gmail.com>
2020-04-22 21:51:49 -0700

    Adds routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const ctrl = require("../controllers");
 
 // PATH = /api/v1/auth
-router.post("/register", ctrl.auth.register);
-router.post("/login", ctrl.auth.login);
-router.get("/verify", ctrl.auth.verify);
-router.delete("/logout", ctrl.auth.logout);
+router.post("/register", ctrl.authCtrl.register);
+router.post("/login", ctrl.authCtrl.login);
+router.get("/verify", ctrl.authCtrl.verify);
+router.delete("/logout", ctrl.authCtrl.logout);
 
 module.exports = router;
