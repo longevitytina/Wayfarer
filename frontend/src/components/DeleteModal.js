ef0496aa0a6f2ebcaa3b4a1a59ca2f393e41b0d2
anya-pich <anna.pich@outlook.com>
2020-04-26 16:14:59 -0700

    confirm delete works

---
+++ frontend/src/components/DeleteModal.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+function DeleteModal(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="danger" onClick={handleShow}>
+        Delete post
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+
+					Are you sure you want to delete post {props.post}?
+
+				</Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={props.delete}>
+            Delete
+          </Button>
+					<Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+// render(<DeleteModal />);
+
+export default DeleteModal;
