cb3700f495d9932d5aef6835269071290744e56d
Tina <longevity.tina@gmail.com>
2020-04-25 18:34:53 -0700

    Adds dynamic linking

--- frontend/src/components/CityPosts.js
+++ frontend/src/components/CityPosts.js
@@ -2,20 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function CityPosts(props) {
-  console.log(props._id);
-
   return (
-    <li className="media py-3">
+    <li className="media position-relative my-4">
       <img className="mr-3 img-thumbnail w-25" src={props.image} alt="" />
       <div className="media-body">
+        <h5 className="mt-0">{props.title}</h5>
+        <p>{props.body[0]}</p>
         <Link
           to={{
             pathname: `/post/${props._id}`,
           }}
         >
-          <h5 className="mt-0 mb-1">{props.title}</h5>
+          <p className="stretched-link mt-0 mb-1"> ...Read more</p>
         </Link>
-        {props.body[0]}...
       </div>
     </li>
   );
