60e9b60dc6310cf97399ddc4001ad119add92a73
eapatton <eapatton91@gmail.com>
2020-04-25 23:15:14 -0700

    post routes updates, needs to debug

--- frontend/src/components/EditProfile.js
+++ frontend/src/components/EditProfile.js
@@ -1,59 +1,62 @@
-import React, { Component } from 'react'
-import UserModel from '../models/user'
-import axios from "axios"
+import React, { Component } from "react";
+import UserModel from "../models/user";
+import axios from "axios";
 
 class EditProfile extends Component {
   state = {
     name: "",
-		city: "",
-		user: "",
-		cities: []
-	}
+    city: "",
+    user: "",
+    cities: [],
+  };
 
-	componentDidMount() {
-		axios
+  componentDidMount() {
+    axios
       .get("http://localhost:3001/api/v1/cities")
-      .then((res) => {this.setState({
-				name: this.props.location.state.name,
-				user: this.props.currentUser,
-				cities: res.data,
-			});
-        // console.log(res.data);
+      .then((res) => {
+        this.setState({
+          name: this.props.location.state.name,
+          user: this.props.currentUser,
+          cities: res.data,
+        });
+        console.log(res.data);
       })
-      .catch(error => console.log("Error fetching and parsing data", error));
-	}
+      .catch((error) => console.log("Error fetching and parsing data", error));
+  }
 
   handleChange = (event) => {
     this.setState({
-        [event.target.name]: event.target.value
-    })
-	}
-	
-	handleSelect = (event) => {
-		if (event.target.value) {
-			const found = this.state.cities.find(city => city.name === event.target.value);
-		// console.log(event.target.value);
-		// console.log(found._id);
-		this.setState({city: found._id})
-		} else {
-			this.setState({city: ""})
-		}
-	}
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSelect = (event) => {
+    if (event.target.value) {
+      const found = this.state.cities.find(
+        (city) => city.name === event.target.value
+      );
+      // console.log(event.target.value);
+      // console.log(found._id);
+      this.setState({ city: found._id });
+    } else {
+      this.setState({ city: "" });
+    }
+  };
 
   handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     UserModel.update(this.state)
-      .then(res => {
+      .then((res) => {
         this.setState({
-          name: '',
-					// city: '',
-					user: ''
-				})
-				console.log(res);
-        this.props.history.push('/profile')
+          name: "",
+          // city: '',
+          user: "",
+        });
+        console.log(res);
+        this.props.history.push("/profile");
       })
-      .catch(err => console.log(err))
-	}
+      .catch((err) => console.log(err));
+  };
 
   render() {
     return (
@@ -62,30 +65,35 @@ class EditProfile extends Component {
           <div className="col-md-4 offset-md-4">
             <h4 className="mb-3">EditProfile</h4>
             <form onSubmit={this.handleSubmit}>
-              
-							<div className="form-group">
+              <div className="form-group">
                 <label htmlFor="name">Name</label>
-                <input 
-                    onChange={this.handleChange} 
-                    className="form-control form-control-lg" 
-                    type="text" 
-                    id="name" 
-                    name="name" 
-                    value={this.state.name}
+                <input
+                  onChange={this.handleChange}
+                  className="form-control form-control-lg"
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={this.state.name}
                 />
               </div>
 
-							<div className="form-group">
-								<label htmlFor="exampleFormControlSelect1">City</label>
-								<select onChange={this.handleSelect} className="form-control" id="exampleFormControlSelect1">
-										<option key="null"></option>
-									{this.state.cities.map(city => (
-										<option key={city._id}>{city.name}</option>
-									))}
-								</select>
-							</div>
+              <div className="form-group">
+                <label htmlFor="exampleFormControlSelect1">City</label>
+                <select
+                  onChange={this.handleSelect}
+                  className="form-control"
+                  id="exampleFormControlSelect1"
+                >
+                  <option key="null"></option>
+                  {this.state.cities.map((city) => (
+                    <option key={city._id}>{city.name}</option>
+                  ))}
+                </select>
+              </div>
 
-              <button className="btn btn-primary float-right" type="submit">EditProfile</button>
+              <button className="btn btn-primary float-right" type="submit">
+                EditProfile
+              </button>
             </form>
           </div>
         </div>
--- frontend/src/models/post.js
+++ frontend/src/models/post.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 export default class PostModel {
-  static remove = (id) => {
+  static remove(id) {
     return axios.delete(`http://localhost:3001/api/v1/posts/${id}`);
-  };
+  }
 
-  static updatePost(id) {
-    return axios.put(`http://localhost:3001/api/v1/posts/${id}`);
+  static update(data) {
+    return axios.put(`http://localhost:3001/api/v1/posts/${data.id}`, data);
   }
 }
+
+// static update(data) {
+//   let request = axios.put(`${REACT_APP_API_URL}/users/${data.user}`, data)
+//   return request
+// }
