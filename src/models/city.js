ef0e47ca99e25176d04b1a73848844bed4240896
Tina <longevity.tina@gmail.com>
2020-04-28 12:30:54 -0700

    Connects heroku database

--- src/models/city.js
+++ src/models/city.js
@@ -1,31 +1,31 @@
-import axios from 'axios'
-
-const REACT_APP_API_URL = "http://localhost:3001/api/v1"
+import axios from "axios";
 
 export default class CityModel {
-
-	static getAll() {
-		let request = axios.get(`${REACT_APP_API_URL}/cities`)
-		return request
-	}
+  static getAll() {
+    let request = axios.get(`${process.env.REACT_APP_API_URL}/cities`);
+    return request;
+  }
 
   static post(data) {
-    let request = axios.post(`${REACT_APP_API_URL}/cities`, data)
-    return request
+    let request = axios.post(`${process.env.REACT_APP_API_URL}/cities`, data);
+    return request;
   }
 
   static put(data) {
-    let request = axios.put(`${REACT_APP_API_URL}/cities/${data._id}`, data)
-    return request
+    let request = axios.put(
+      `${process.env.REACT_APP_API_URL}/cities/${data._id}`,
+      data
+    );
+    return request;
   }
 
   static getOne(id) {
-    let request = axios.get(`${REACT_APP_API_URL}/cities/${id}`)
-    return request
-	}
-	
-	static delete(id) {
-    let request = axios.delete(`${REACT_APP_API_URL}/cities/${id}`)
-    return request
+    let request = axios.get(`${process.env.REACT_APP_API_URL}/cities/${id}`);
+    return request;
+  }
+
+  static delete(id) {
+    let request = axios.delete(`${process.env.REACT_APP_API_URL}/cities/${id}`);
+    return request;
   }
 }
--- src/models/post.js
+++ src/models/post.js
@@ -1,43 +1,45 @@
 import axios from "axios";
 
-const REACT_APP_API_URL = "http://localhost:3001/api/v1";
-
 export default class PostModel {
   static getAll() {
-    let request = axios.get(`${REACT_APP_API_URL}/posts`);
+    let request = axios.get(`${process.env.REACT_APP_API_URL}/posts`);
     return request;
   }
 
   static getByAuthor(id) {
-    let request = axios.get(`${REACT_APP_API_URL}/posts?author=${id}`);
+    let request = axios.get(
+      `${process.env.REACT_APP_API_URL}/posts?author=${id}`
+    );
     return request;
   }
 
   static getByCity(id) {
-    let request = axios.get(`${REACT_APP_API_URL}/posts?city=${id}`);
+    let request = axios.get(
+      `${process.env.REACT_APP_API_URL}/posts?city=${id}`
+    );
     return request;
   }
 
   static post(data) {
-    let request = axios.post(`${REACT_APP_API_URL}/posts`, data);
+    let request = axios.post(`${process.env.REACT_APP_API_URL}/posts`, data);
     return request;
   }
 
   static put(data) {
     let request = axios.put(
-      `${REACT_APP_API_URL}/posts/${data.data._id}`,
+      `${process.env.REACT_APP_API_URL}/posts/${data.data._id}`,
       data
     );
     return request;
   }
 
   static getOne(id) {
-    let request = axios.get(`${REACT_APP_API_URL}/posts/${id}`);
+    let request = axios.get(`${process.env.REACT_APP_API_URL}/posts/${id}`);
     return request;
   }
 
   static delete(id) {
-    let request = axios.delete(`${REACT_APP_API_URL}/posts/${id}`);
+    let request = axios.delete(`${process.env.REACT_APP_API_URL}/posts/${id}`);
     return request;
   }
 }
